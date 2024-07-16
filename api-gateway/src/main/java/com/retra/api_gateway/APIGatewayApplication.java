a1cfdbcaa3cdfea0dad37d8521cf88d714ecd11e
marsRocha <marciorocha.dev@gmail.com>
2024-07-16 19:57:15 +0100

    Rename gateway service to api-gateway

--- api-gateway/src/main/java/com/retra/api_gateway/APIGatewayApplication.java
+++ api-gateway/src/main/java/com/retra/api_gateway/APIGatewayApplication.java
@@ -1,13 +1,13 @@
-package com.retra.gateway;
+package com.retra.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class APIGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
+		SpringApplication.run(APIGatewayApplication.class, args);
 	}
 
 }
--- api-gateway/src/test/java/com/retra/api_gateway/APIGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/retra/api_gateway/APIGatewayApplicationTests.java
@@ -1,10 +1,10 @@
-package com.retra.gateway;
+package com.retra.api_gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GatewayApplicationTests {
+class APIGatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
